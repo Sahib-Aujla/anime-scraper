fb08675dd3ded14ee46d4bb15ef4a1139dc1a71b
Sahib.Aujla <sahibpreet568@gmail.com>
2025-01-29 13:22:32 -0500

    added search

--- web-scraper.js
+++ web-scraper.js
@@ -40,5 +40,40 @@ export async function getRecentReleases(page = "1") {
   }
 }
 
+export async function searchAnime(keyword) {
+  try {
+    const res = [];
 
-console.log(await getRecentReleases('2'));
+    const response = await axios.get(
+      `${base_url}/search.html?keyword=${keyword}`
+    );
+    const $ = cheerio.load(response.data);
+
+    const items = $("div.last_episodes ul.items li");
+
+    items.each((index, element) => {
+      const $el = $(element);
+
+      const link = $el.find("div.img a").attr("href") || "";
+      const title = $el.find("div.img a").attr("title") || "";
+      const image = $el.find("div.img a img").attr("src") || "";
+      const animeName = $el.find("p.name a").text().trim();
+      const released = $el.find("p.released").text().trim();
+
+      res.push({
+        link,
+        title,
+        image,
+        animeName,
+        released,
+      });
+    });
+
+    return res;
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+    return [];
+  }
+}
+
+console.log(await searchAnime("solo"));
