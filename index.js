6526999e2e6f5a88b2ac272bf8790cfd86387a77
Sahib.Aujla <sahibpreet568@gmail.com>
2025-01-29 13:31:28 -0500

    added listen and update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import apiRoutes from './routes/index.js';
+import express from "express";
+import cors from "cors";
+import apiRoutes from "./routes/index.js";
 const app = express();
 app.use(cors());
 
+app.get("/v1/", apiRoutes);
 
-app.get('/v1/',apiRoutes);
+app.listen(8080, () => {
+  console.log("listening on port 8080");
+});
